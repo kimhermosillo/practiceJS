28acb6882afe258e0368b827063e4f631d996219
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-10-15 15:02:39 -0500

    ternary operators and switch statements

--- basics.js
+++ basics.js
@@ -8,6 +8,13 @@ var job = 'software developer'
 // boolean logic but also else / if statements.
 if (age < 20 ) {
   console.log(firstName + ' is a girl.')
-} else {
+} else if (age >= 13 && age < 20) {
+  console.log(firstName + ' ' + 'is a teenager.')
+} else  {
   console.log(firstName + ' ' + 'is a lady.')
 }
+// Temporary Operater and Switch Statements.
+age >= 21 ? console.log(firstName + ' drinks beer.')
+  : console.log(firstName + 'drinks juice.')
+
+var drink = age >= 21 ? 'beer' : 'juice'
