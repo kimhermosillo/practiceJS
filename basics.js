452af21a098b7311322dac3c70907b3094b6d2de
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-10-15 14:36:25 -0500

    stated some variables

---
+++ basics.js
@@ -0,0 +1,13 @@
+console.log('we all finna die anyways!!!!')
+
+var firstName = 'Kim'
+var lastName = 'Hermosillo'
+var age = 22
+var job = 'software developer'
+
+// boolean logic but also else / if statements.
+if (age < 20 ) {
+  console.log(firstName + ' is a girl.')
+} else {
+  console.log(firstName + ' ' + 'is a lady.')
+}
