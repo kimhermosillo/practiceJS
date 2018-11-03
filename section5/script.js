8eb7e4f99ab03b0de657fa6a20d2a651e6bb6fff
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-11-02 21:01:39 -0600

    Primitive vs Objects

--- section5/script.js
+++ section5/script.js
@@ -35,7 +35,7 @@ console.log(kim.lastName)
 //they all inherit Hermosillo as a last name due to the prototype.
 */
 //CREATING OBJECTS
-
+/*
 var personProto = {
   calculateAge: function() {
     console.log(2018 - this.yearOfBirth)
@@ -53,3 +53,37 @@ var martin = Object.create(personProto, {
   yearOfBirth: { value: 1985 },
   job: { value: 'lover' },
 })
+*/
+
+// PRIMITIVE VS OBJECTS
+//PRIMITIVES
+var a = 69
+var b = a
+a = 420
+console.log(a)
+console.log(b)
+// OBJECTS
+var obj1 = {
+  name: 'Kim',
+  age: 22,
+}
+var obj2 = obj1
+obj1.age = 21
+console.log(obj1.age)
+console.log(obj2.age)
+// FUNCTIONS
+var age = 22
+var obj = {
+  name: 'Kim',
+  city: 'Mexico City',
+}
+
+function change(a, b) {
+  a = 22,
+  b.city = 'Oaxaca'
+}
+
+change(age, obj)
+
+console.log(age)
+console.log(obj.city)
