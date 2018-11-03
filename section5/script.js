1191768492e5457cc6a8cde24fb68b912856ce57
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-11-02 21:20:03 -0600

    Functions returning functions - halfway

--- section5/script.js
+++ section5/script.js
@@ -88,7 +88,7 @@ change(age, obj)
 console.log(age)
 console.log(obj.city)
 */
-
+/*
 //LECTURE; PASSING FUNCTIONS AS ARGUMENTS
 
 var years = [1985, 1992, 1996, 1999, 2017]
@@ -122,3 +122,26 @@ var heartRate = arrayCalc(ages, maxHeartRate)
 console.log(ages)
 console.log(fullAges)
 console.log(heartRate)
+*/
+
+//FUNCTIONS RETURNING FUNCTIONS
+
+function interviewQuestion(job) {
+  if (job === 'developer') {
+    return function(name) {
+      console.log(name + ', can you please explain what UX design is?')
+    }
+  } else if (job === 'teacher') {
+    return function(name) {
+      console.log('What subject do you teach, ' + name + '?')
+    }
+  } else {
+    return function(name) {
+      console.log('Hello ' + name + ', what do you do?')
+    }
+  }
+}
+
+var teacherQuestion = interviewQuestion('teacher')
+
+teacherQuestion('Kim')
