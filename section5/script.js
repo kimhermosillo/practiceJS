d39e740adfdf679e9dd2ac33e2c1db3845b613e0
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-11-02 20:52:48 -0600

    WIP: using Object.create

--- section5/script.js
+++ section5/script.js
@@ -1,5 +1,5 @@
 // FUNCTION CONSTRUCTOR
-var kim = {
+/*var kim = {
   name: 'Kim',
   yearOfBirth: 1996,
   job: 'developer',
@@ -18,6 +18,8 @@ Person.prototype.calculateAge = function() {
   console.log(2018-yearOfBirth)
 }
 
+Person.prototype.lastName = 'Hermosillo'
+
 
 var kim = new Person('Kim', 1996, 'developer')
 var martin = new Person('Martin', 1985, 'jiujitsu')
@@ -27,3 +29,27 @@ var amanda = new Person('Amanda', 1995, 'cruiser')
 kim.calculateAge()
 martin.calculateAge()
 amanda.calculateAge()
+
+console.log(kim.lastName)
+
+//they all inherit Hermosillo as a last name due to the prototype.
+*/
+//CREATING OBJECTS
+
+var personProto = {
+  calculateAge: function() {
+    console.log(2018 - this.yearOfBirth)
+  },
+}
+
+var kim = Object.create(personProto)
+
+kim.name = 'Kim'
+kim.yearOfBirth = 1996
+kim.job = 'developer'
+
+var martin = Object.create(personProto, {
+  name: { value: 'Martin' },
+  yearOfBirth: { value: 1985 },
+  job: { value: 'lover' },
+})
