9d2a8f3e9344c05a8865e1626fea264b3427a2d6
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-11-02 20:34:15 -0600

    WIP: Learned about inheritance

--- section5/script.js
+++ section5/script.js
@@ -1,8 +1,29 @@
 // FUNCTION CONSTRUCTOR
-
 var kim = {
   name: 'Kim',
   yearOfBirth: 1996,
   job: 'developer',
 }
 
+var Person = function(name, yearOfBirth, job) {
+  this.name = name
+  this.yearOfBirth = yearOfBirth
+  this.job = job
+  this.calculateAge = function() {
+    console.log(2018-yearOfBirth)
+  }
+}
+
+Person.prototype.calculateAge = function() {
+  console.log(2018-yearOfBirth)
+}
+
+
+var kim = new Person('Kim', 1996, 'developer')
+var martin = new Person('Martin', 1985, 'jiujitsu')
+var amanda = new Person('Amanda', 1995, 'cruiser')
+
+
+kim.calculateAge()
+martin.calculateAge()
+amanda.calculateAge()
