4a991e10bc5aa0d81dd3973c5e81ca4018d9c6a9
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-11-04 13:13:00 -0600

    Fixed indentation issues.

--- budgetApp/app.js
+++ budgetApp/app.js
@@ -1,63 +1,66 @@
 // BUDGET CONTROLLER
 
-var budgetController = (function() {
+var budgetController = (function () {
 
-  // some code
+    // some code
 
 })()
 // UI CONTROLLER
-var UIController = (function() {
+var UIController = (function () {
 
     var DOMstrings = {
-        inputType: '.add__type', 
+        inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
         inputBtn: '.add__btn',
     }
 
-  return {
-    getinput: function() {
-       type: document.querySelector(DOMstrings.inputType).value,
-       description: document.querySelector(DOMstrings.inputDescription).value,
-       value: document.querySelector(DOMstrings.inputValue).value,
+    return {
+        getinput: function () {
+            type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                    value: document.querySelector(DOMstrings.inputValue).value,
     }
-  }
-getDOMstrings: function() {
-    return DOMstrings
-}
+    }
+    getDOMstrings: function() {
+        return DOMstrings
+    }
 })()
 
 // GLOBAL APP CONTROLLER
-var controller = (function(budgetController, UIController) {
+var controller = (function (budgetController, UIController) {
 
-var DOM = UIController.getDOMstrings()
+    var setupEventListeners = function () {
+        var DOM = UIController.getDOMstrings()
 
-  var ctrlAddItem = function () {
-    // 1. GET THE FILLED INPUT DATA
-    var input = UIController.getinput()
-    console.log(input)
-    //2. ADD THE ITEM TO THE BUDGET CONTROLLER
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
 
-    //3. ADD THE NEW ITEM TO UI
-
-    //4. CALCULATE THE BUDGET
-
-    //5. DISPLAY THE BUDGET ON THE UI
-    
-  }
-
-  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
-
-  document.addEventListener('keypress', function(event) {
-     
-    if (event.keyCode === 13 || event.which === 13) {
-      ctrlAddItem()
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem()
+            }
+        })
     }
 
-  })
 
 
-})(budgetController, UIController)
+    var ctrlAddItem = function () {
+        // 1. GET THE FILLED INPUT DATA
+        var input = UIController.getinput()
+        console.log(input)
+        //2. ADD THE ITEM TO THE BUDGET CONTROLLER
+
+        //3. ADD THE NEW ITEM TO UI
+
+        //4. CALCULATE THE BUDGET
+
+        //5. DISPLAY THE BUDGET ON THE UI
+
+    }
+
+
+
+}) (budgetController, UIController)
 
 
 
