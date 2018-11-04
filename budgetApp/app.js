61b01367659fb89da201cb8504ae26ceb5b8d901
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-11-04 12:46:07 -0600

    WIP: Added first event listeners

--- budgetApp/app.js
+++ budgetApp/app.js
@@ -1,18 +1,11 @@
+// BUDGET CONTROLLER
+
 var budgetController = (function() {
 
-  var x = 23
-  var add = function (a) {
-    return x + a
-  }
-
-  return {
-    publicTest: function(b) {
-      console.log(add(b))
-    },
-  }
+  // some code
 
 })()
-
+// UI CONTROLLER
 var UIController = (function() {
 
 
@@ -20,16 +13,35 @@ var UIController = (function() {
 
 })()
 
-
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetController, UIController) {
 
-  var z = budgetController.publicTest(5)
+  var ctrlAddItem = function () {
+    // 1. GET THE FILLED INPUT DATA
 
-  return {
-    anotherPublic: function() {
-      console.log(z)
-    },
+    //2. ADD THE ITEM TO THE BUDGET CONTROLLER
+
+    //3. ADD THE NEW ITEM TO UI
+
+    //4. CALCULATE THE BUDGET
+
+    //5. DISPLAY THE BUDGET ON THE UI
+    console.log('it works')
   }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+  document.addEventListener('keypress', function(event) {
+     
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem()
+    }
+
+  })
 
 
 })(budgetController, UIController)
+
+
+
+//
