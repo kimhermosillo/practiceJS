636d9a4e6f44065e09c98b416e4ca786ab121c36
Kimberly Hermosillo <kimhermosillo@yahoo.com>
2018-11-04 13:57:43 -0600

    WIP: Added new items to budget controller

--- budgetApp/app.js
+++ budgetApp/app.js
@@ -2,13 +2,13 @@
 
 var budgetController = (function () {
 
-    var Expense = function(id, description, value) {
+    var Expense = function (id, description, value) {
         this.id = id
         this.description = description
         this.value = value
     }
 
-    var Income = function(id, description, value) {
+    var Income = function (id, description, value) {
         this.id = id
         this.description = description
         this.value = value
@@ -27,7 +27,34 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
+        }
+    }
 
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID
+
+            // [1 2 3 4 5], next ID = 6
+//CREATE NEW ID
+            if(data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1
+            } else {
+                ID = 0
+            }
+        
+// CREATE NEW ITEM BASED ON IN OR EXP TYPE
+            if(type === 'exp') {
+            newItem = new Expense(ID, des, val)
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val)
+            }
+            // PUSH IT INTO DATA STRUCTURE
+            data.allItems[type].push(newItem)
+            // RETURN THE NEW ELEMENT
+            return newItem
+        },
+        testing: function() {
+            console.log(data)
         }
     }
 
@@ -46,14 +73,16 @@ var UIController = (function () {
 
     return {
         getinput: function () {
-            type: 
-                document.querySelector(DOMstrings.inputType).value 
-                description: document.querySelector(DOMstrings.inputDescription).value
-                value: document.querySelector(DOMstrings.inputValue).value,
-    }
-    }
+            type:
+            document.querySelector(DOMstrings.inputType).value
+            description: document.querySelector(DOMstrings.inputDescription).value
+            value: document.querySelector(DOMstrings.inputValue).value
+        }
+    },
+        
+        
     getDOMstrings: function() {
-        return DOMstrings
+        return DOMstrings;
     }
 })()
 
@@ -75,10 +104,12 @@ var controller = (function (budgetController, UIController) {
 
 
     var ctrlAddItem = function () {
+        var input, newItem
         // 1. GET THE FILLED INPUT DATA
-        var input = UIController.getinput()
-        
+        input = UIController.getinput()
+
         //2. ADD THE ITEM TO THE BUDGET CONTROLLER
+        newItem = budgetController.addItem(input.type, input.description, input.value)
 
         //3. ADD THE NEW ITEM TO UI
 
@@ -88,14 +119,14 @@ var controller = (function (budgetController, UIController) {
 
     }
 
-return {
-    init: function() {
-        console.log('Application has started.')
-        setupEventListeners()
+    return {
+        init: function () {
+            console.log('Application has started.')
+            setupEventListeners()
+        }
     }
-}
 
-}) (budgetController, UIController)
+})(budgetController, UIController)
 
 controller.init()
 
